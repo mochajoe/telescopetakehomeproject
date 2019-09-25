a803405ee6b955b07d02fb2bf35a9f4e52fe35cb
Anthony Nguyen <anguyee@gmu.edu>
2019-09-24 23:51:03 -0700

    Removed fb login and created a header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+    render() {
+        return (
+    <header className = "header-main">
+        <div className = "inner">sdfsdfsd</div>
+    </header>
+
+        )
+    }
+}
+
+export default Header;
